5e91531b048a79eeae89b1fa668dce879bf8fce9
ShivamSh002 <shivamsharma98879@gmail.com>
2024-08-05 22:47:44 +0530

    Navbar completed

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,155 @@
+import styles from "./NavBar.module.css"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import Resume from "../Assests/cv.pdf"
+
+
+const pages = ['Home', 'About Me', 'Projects','Contact Me'];
+
+
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+ 
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+
+
+  return (
+    <AppBar position="static" 
+    sx={{ 
+      background: 'linear-gradient(180deg, #121212 0%, #121212 100%)'
+    }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+        
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              textDecoration: 'none',
+            
+            }}
+            className={styles.logo}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+             
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+          
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' } ,
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              
+              textDecoration: 'none',
+             
+            }}
+            className={styles.logo}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } , justifyContent : "center" }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block',mx :2 }}
+                className={styles.subHeading}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Button 
+  variant="contained" 
+  sx={{ 
+    background: 'linear-gradient(90deg, #FF8C00 50%, #FF4500)',
+    '&:hover': {
+      background: 'linear-gradient(90deg, #FF8C00, #FF4500)',
+    }
+  }}
+>
+<a 
+   href={Resume} 
+   download="ShivamSharma.pdf"  
+    style={{ textDecoration: 'none', color: 'inherit' }}
+  >
+    DOWNLOAD CV
+  </a>
+</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default NavBar;
