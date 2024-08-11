a5b2699fd6e29d065d46b8be4b9cbe54774159ea
ShivamSh002 <shivamsharma98879@gmail.com>
2024-08-11 18:12:48 +0530

    swiper completed

--- src/Projects/Swiper.jsx
+++ src/Projects/Swiper.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Img from "../Assests/image.png";
+import Qkart from "../Assests/qkart.png";
+import Game from "../Assests/game.png";
+import Qtify from '../Assests/qtify.png';
+import Xboard from "../Assests/xboard.png";
+import LawFirm from "../Assests/lawFirm.png";
+import Qtrip from "../Assests/qtrip.png";
+import QtripS from "../Assests/qtripS.png";
+import Data from "../Data/data.json"
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 
-import './Swiper.css';
+// Import required modules
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
 
-// import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import './Swiper.css';
 
 export default function SwiperGrid() {
   return (
@@ -20,27 +27,35 @@ export default function SwiperGrid() {
         grabCursor={true}
         centeredSlides={true}
         slidesPerView={'auto'}
-        modules={[EffectCoverflow, Pagination]}
+        pagination={true}
+        loop={true} 
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
         className="mySwiper"
-        style={{ width: '100%', height: '500px' }} // Adjust this value according to your image height
       >
-        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' ,height :"180px" }}>
-          <img src={Img} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
+         <a href="https://qkart-frontend-9l0j8a0z8-shivam-sharmas-projects-1836c282.vercel.app/" target='blank'> <img src={Qkart} id='2' style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} alt="Qkart" /></a>
         </SwiperSlide>
-        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',height :"180px" }}>
-          <img src={Img} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
+          <a href="https://matching-game-project-delta.vercel.app/" target='blank'><img src={Game} id='7' style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} alt="Game" /></a>
         </SwiperSlide>
-        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',height :"180px" }}>
-          <img src={Img} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
+          <a href="https://qtify-nine-zeta.vercel.app/" target='blank'>  <img src={Qtify}id='1' style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} alt="Qtify" /></a>
         </SwiperSlide>
-        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',height :"180px" }}>
-          <img src={Img} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
+         <a href="https://law-firm-coral.vercel.app/" target='blank'> <img src={LawFirm} id='5' style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} alt="Law Firm" /></a>
         </SwiperSlide>
-        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',height :"180px" }}>
-          <img src={Img} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
+          <a href="https://shivamsharma-crionewsfeed.netlify.app/" target='blank'><img src={Xboard} id='6' style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} alt="Xboard" /></a>
         </SwiperSlide>
-        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',height :"180px" }}>
-          <img src={Img} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
+          <a href="https://qtrip-dynamic-an92cmvb2-shivam-sharmas-projects-1836c282.vercel.app/" target='blank'><img src={Qtrip} id="3" style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} alt="Qtrip" /></a>
+        </SwiperSlide>
+        <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
+         <a href="https://shivamsharma-qtri.netlify.app/" target='blank'> <img src={QtripS} id='4' style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} alt="QtripS" /></a>
         </SwiperSlide>
       </Swiper>
     </>
